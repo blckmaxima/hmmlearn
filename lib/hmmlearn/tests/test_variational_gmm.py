073752b2e13d3355c71fd822867c0cdf9dacfe5f
Matthew Danielson <matt.danielson@gmail.com>
2023-02-09 19:47:02 -0800

    more tests

--- lib/hmmlearn/tests/test_variational_gmm.py
+++ lib/hmmlearn/tests/test_variational_gmm.py
@@ -64,6 +64,7 @@ def prep_params(n_comps, n_mix, n_features, covar_type,
 
     return covs, means, startprob, transmat, weights
 
+
 class GaussianLikeMixin:
     n_components = 3
     n_mix = 1
@@ -117,6 +118,22 @@ class TestVariationalGMMHMMWithFullCovars(GaussianLikeMixin):
     covariance_type = "full"
 
 
+class TestVariationalGMMHMMWithDiagCovars(GaussianLikeMixin):
+    covariance_type = "diag"
+
+# For a Gaussian HMM, Tied covariance means all HMM States share
+# one Covariance Matrix. For a GMM HMM, Tied covariance means all
+# mixture components within a state share one Covariance Matrix.
+# So it does not make sense to compare them two models
+
+# class TestVariationalGMMHMMWithTied(GaussianLikeMixin):
+#     covariance_type = "tied"
+#
+
+class TestVariationalGMMHMMWithSphericalCovars(GaussianLikeMixin):
+    covariance_type = "spherical"
+
+
 class VariationalGMMHMMTestMixin:
     n_components = 3
     n_mix = 2
@@ -128,7 +145,7 @@ class VariationalGMMHMMTestMixin:
                    n_mix=self.n_mix,
                    covariance_type=self.covariance_type,
                    random_state=None,
-                   implementation=implementation, verbose=True, tol=1e-6)
+                   implementation=implementation, tol=1e-6)
 
     def new_hmm_to_sample(self, implementation):
         prng = np.random.RandomState(44)
@@ -198,7 +215,6 @@ class VariationalGMMHMMTestMixin:
         X, states = source.sample(n_samples, random_state=32)
         self.do_test_learn(implementation, X, [n_samples //4] * 4)
 
-
 #    @pytest.mark.parametrize("implementation", ["scaling", "log"])
 #    def test_sample(self, implementation):
 #        n_samples = 1000
@@ -227,63 +243,39 @@ class VariationalGMMHMMTestMixin:
 #        _viterbi_ll, decoded_states = h.decode(X)
 #        assert_allclose(states, decoded_states)
 #
-#    @pytest.mark.parametrize("implementation", ["scaling", "log"])
-#    def test_fit(self, implementation):
-#        n_iter = 5
-#        n_samples = 1000
-#        lengths = None
-#        h = self.new_hmm(implementation)
-#        X, _state_sequence = h.sample(n_samples)
-#
-#        # Mess up the parameters and see if we can re-learn them.
-#        pbif0, zrnaf0, cevbef0, genaf0, jrvtugf0 = cerc_cnenzf(
-#            frys.a_pbzcbaragf, frys.a_zvk, frys.a_srngherf,
-#            self.covariance_type, self.low, self.high,
-#            np.random.RandomState(15)
-#        )
-#        h.covars_ = covs0 * 100
-#        h.means_ = means0
-#        h.startprob_ = priors0
-#        h.transmat_ = trans0
-#        h.weights_ = weights0
-#        assert_log_likelihood_increasing(h, X, lengths, n_iter)
-#
-#    @pytest.mark.parametrize("implementation", ["scaling", "log"])
-#    def test_fit_sparse_data(self, implementation):
-#        n_samples = 1000
-#        h = self.new_hmm(implementation)
-#        h.means_ *= 1000  # this will put gaussians very far apart
-#        X, _states = h.sample(n_samples)
-#
-#        # this should not raise
-#        # "ValueError: array must not contain infs or NaNs"
-#        h._init(X, [1000])
-#        h.fit(X)
-#
-#    @pytest.mark.xfail
-#    @pytest.mark.parametrize("implementation", ["scaling", "log"])
-#    def test_fit_zero_variance(self, implementation):
-#        # Example from issue #2 on GitHub.
-#        # this data has singular covariance matrix
-#        X = np.asarray([
-#            [7.15000000e+02, 5.8500000e+02, 0.00000000e+00, 0.00000000e+00],
-#            [7.15000000e+02, 5.2000000e+02, 1.04705811e+00, -6.03696289e+01],
-#            [7.15000000e+02, 4.5500000e+02, 7.20886230e-01, -5.27055664e+01],
-#            [7.15000000e+02, 3.9000000e+02, -4.57946777e-01, -7.80605469e+01],
-#            [7.15000000e+02, 3.2500000e+02, -6.43127441e+00, -5.59954834e+01],
-#            [7.15000000e+02, 2.6000000e+02, -2.90063477e+00, -7.80220947e+01],
-#            [7.15000000e+02, 1.9500000e+02, 8.45532227e+00, -7.03294373e+01],
-#            [7.15000000e+02, 1.3000000e+02, 4.09387207e+00, -5.83621216e+01],
-#            [7.15000000e+02, 6.5000000e+01, -1.21667480e+00, -4.48131409e+01]
-#        ])
-#
-#        h = self.new_hmm(implementation)
-#        h.fit(X)
-#
-#
-#     covariance_type = 'spherical'
-#
-#
+    @pytest.mark.parametrize("implementation", ["scaling", "log"])
+    def test_fit_sparse_data(self, implementation):
+        n_samples = 1000
+        h = self.new_hmm_to_sample(implementation)
+        h.means_ *= 1000  # this will put gaussians very far apart
+        X, _states = h.sample(n_samples)
+
+        m = self.new_hmm_to_sample(implementation)
+        # this should not raise
+        # "ValueError: array must not contain infs or NaNs"
+        h.fit(X)
+
+    @pytest.mark.xfail
+    @pytest.mark.parametrize("implementation", ["scaling", "log"])
+    def test_fit_zero_variance(self, implementation):
+        # Example from issue #2 on GitHub.
+        # this data has singular covariance matrix
+        X = np.asarray([
+            [7.15000000e+02, 5.8500000e+02, 0.00000000e+00, 0.00000000e+00],
+            [7.15000000e+02, 5.2000000e+02, 1.04705811e+00, -6.03696289e+01],
+            [7.15000000e+02, 4.5500000e+02, 7.20886230e-01, -5.27055664e+01],
+            [7.15000000e+02, 3.9000000e+02, -4.57946777e-01, -7.80605469e+01],
+            [7.15000000e+02, 3.2500000e+02, -6.43127441e+00, -5.59954834e+01],
+            [7.15000000e+02, 2.6000000e+02, -2.90063477e+00, -7.80220947e+01],
+            [7.15000000e+02, 1.9500000e+02, 8.45532227e+00, -7.03294373e+01],
+            [7.15000000e+02, 1.3000000e+02, 4.09387207e+00, -5.83621216e+01],
+            [7.15000000e+02, 6.5000000e+01, -1.21667480e+00, -4.48131409e+01]
+        ])
+
+        h = self.new_hmm(implementation)
+        h.fit(X)
+
+
 class TestVariationalGMMHMMWithSphericalCovars(VariationalGMMHMMTestMixin):
     covariance_type = 'spherical'
 
@@ -295,4 +287,4 @@ class TestVariationalGMMHMMWithFullCovars(VariationalGMMHMMTestMixin):
 
 class TestVariationalGMMHMMWithTiedCovars(VariationalGMMHMMTestMixin):
     covariance_type = 'tied'
-
+#
